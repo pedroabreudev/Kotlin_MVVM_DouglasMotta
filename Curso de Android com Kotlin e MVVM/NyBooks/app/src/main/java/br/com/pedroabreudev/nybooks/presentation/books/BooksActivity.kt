94e9b884b6667e960967cdb17a9de6a2ecae0a54
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-10-19 14:37:54 -0400

    Curso de Android com Kotlin e MVVM #1
    Apresentação do Projeto

---
+++ Curso de Android com Kotlin e MVVM/NyBooks/app/src/main/java/br/com/pedroabreudev/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package br.com.pedroabreudev.nybooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.pedroabreudev.nybooks.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
