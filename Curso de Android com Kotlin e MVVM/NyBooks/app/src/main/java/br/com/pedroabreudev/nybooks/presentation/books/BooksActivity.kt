62594ac3f5084cfa6a2ccce4f9bcff564d12f60c
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-10-31 15:30:33 -0400

    Curso de Android com Kotlin e MVVM #3

--- Curso de Android com Kotlin e MVVM/NyBooks/app/src/main/java/br/com/pedroabreudev/nybooks/presentation/books/BooksActivity.kt
+++ Curso de Android com Kotlin e MVVM/NyBooks/app/src/main/java/br/com/pedroabreudev/nybooks/presentation/books/BooksActivity.kt
@@ -3,10 +3,14 @@ package br.com.pedroabreudev.nybooks.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.pedroabreudev.nybooks.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
     }
 }
