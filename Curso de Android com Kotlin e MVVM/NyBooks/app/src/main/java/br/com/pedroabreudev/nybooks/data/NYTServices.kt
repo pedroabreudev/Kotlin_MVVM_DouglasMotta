cef2976f88cdf8131d30ea9260dbfe912be79f24
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-02 22:13:40 -0400

    Curso de Android com Kotlin e MVVM #7
    Finalizando a Camada de Network

--- Curso de Android com Kotlin e MVVM/NyBooks/app/src/main/java/br/com/pedroabreudev/nybooks/data/NYTServices.kt
+++ Curso de Android com Kotlin e MVVM/NyBooks/app/src/main/java/br/com/pedroabreudev/nybooks/data/NYTServices.kt
@@ -1,9 +1,17 @@
 package br.com.pedroabreudev.nybooks.data
 
+import br.com.pedroabreudev.nybooks.data.response.BookBodyResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NYTServices {
     @GET("lists.json")
-    fun listRepos(): Call<List<>>
+    fun listRepos(
+        @Query("api-key")
+        apiKey: String = "ycDXzkGGUiB1JMm6SbV98NJf6mCJ6zO4",
+
+        @Query("list")
+        list: String = "hardcover-fiction"
+    ): Call<BookBodyResponse>
 }
